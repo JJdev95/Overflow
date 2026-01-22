1aadedc5952ff3a56a85e135868ee30ef935d346
Jovan Josifoski <jovan@antweb.io>
2026-01-22 16:07:55 +0100

    doing some sonarqube suggested improvements

--- QuestionService/Validators/TagListValidatorAttribute.cs
+++ QuestionService/Validators/TagListValidatorAttribute.cs
@@ -2,15 +2,12 @@ using System.ComponentModel.DataAnnotations;
 
 namespace QuestionService.Validators;
 
-public class TagListValidator(int min, int max) : ValidationAttribute
+public class TagListValidatorAttribute(int min, int max) : ValidationAttribute
 {
     protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
     {
-        if (value is List<string> tags)
-        {
-            if (tags.Count >= min && tags.Count <= max) return ValidationResult.Success;
-        }
-        
+        if (value is List<string> tags && tags.Count >= min && tags.Count <= max) return ValidationResult.Success;
+
         return new ValidationResult($"You must provide at least {min} and {max} tags.");
     }
 }
