2e4faa4d7b17687e60e75789e8092c8ec7b84fe0
Jovan Josifoski <jovan@antweb.io>
2026-01-22 15:26:55 +0100

    adding sonarqube yml + first part of section 4

--- Overflow.AppHost/AppHost.cs
+++ Overflow.AppHost/AppHost.cs
@@ -10,6 +10,14 @@ var postgres = builder.AddPostgres("postgres", port: 5432)
     .WithDataVolume("postgres-data")
     .WithPgAdmin();
 
+var typesense = builder.AddContainer("typesense", "typesense/typesense", "29.0")
+    .WithArgs("--data-dir", "/data/", "--api-key", "xyz", "--enable-cors")
+    .WithVolume("typesense-data", "/data")
+    .WithHttpEndpoint(8108, 8108, name: "typesense");
+
+var typesenseContainer = typesense.GetEndpoint("typesense");
+
+
 var questionDb = postgres.AddDatabase("questionDb");
 
 var questionService = builder.AddProject<Projects.QuestionService>("question-svc")
@@ -17,5 +25,9 @@ var questionService = builder.AddProject<Projects.QuestionService>("question-svc
     .WithReference(questionDb)
     .WaitFor(keycloak)
     .WaitFor(questionDb);
+
+var searchService = builder.AddProject<Projects.SearchService>("search-svc")
+    .WithReference(typesenseContainer)
+    .WaitFor(typesense);
 
 builder.Build().Run();
